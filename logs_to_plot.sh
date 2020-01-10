5b0d36cf2a50ce12745f14f2bb3040987c68aafc
Chris Elston <chris.elston@sancloud.co.uk>
2020-01-10 12:35:30 +0000

    Add testing script & plot generation script
    - test_sequence.sh can be used to run a sequence of test cases
      with different numbers of concurrent webthing instances, results
      are logged for analysis by....
    
    - logs_to_plot.sh: plots longest and mean propagation times to
      a graph.

---
+++ logs_to_plot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+grep Mean *.log | sed -n 's/testlog_\([^_]*\)[^:]*[^0-9]*\([0-9.]*\)ms/\1,\2/g; p' | sort -n > mean.csv
+grep Longest *.log | sed -n 's/testlog_\([^_]*\)[^:]*[^0-9]*\([0-9.]*\)ms/\1,\2/g; p' | sort -n > longest.csv
+csvjoin --columns 1 mean.csv longest.csv > logresults.csv
+gnuplot <<- EOF
+set terminal pngcairo nocrop enhanced font "verdana,10" size 1024,768
+set output 'plot.png'
+unset border
+set boxwidth 0.1
+set style fill solid 1.00
+
+#set terminal svg size 410,250 fname 'Verdana, Helvetica, Arial, sans-serif' fsize '9' rounded dashed
+#set output 'plot.svg'
+
+# remove border on top and right and set color to gray
+set style line 11 lc rgb '#808080' lt 1
+set border 3 back ls 11
+set tics nomirror
+
+# define grid
+set style line 12 lc rgb '#808080' lt 0 lw 1
+set grid back ls 12
+
+# color definitions
+set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
+set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2 # --- green
+
+set datafile separator ','
+set ylabel 'Propagation time (ms)'
+set xlabel 'Number of webthings'
+plot 'logresults.csv' using 1:2 t 'mean' w lp ls 1, '' using 1:3 t 'longest' w lp ls 2
+EOF
